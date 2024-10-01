d7e8413c3ecb1667e29adf1ba763fb6c1625f834
Alley Shawwn <cayetuna2000@gmail.com>
2024-10-01 22:56:12 +0800

    revert constant values

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 const ACCENT_PRIMARY = Colors.teal;
 var ACCENT_SECONDARY = Color(0xFF00CDCD);
 const PESO = "₱";
-// const BASE_URL = "https://broiler-counter.vercel.app";
-const BASE_URL = "https://0eaa-222-127-93-21.ngrok-free.app";
+const BASE_URL = "https://broiler-counter.vercel.app";
+// const BASE_URL = "https://18e2-222-127-93-21.ngrok-free.app";
